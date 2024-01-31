6fb156e0a5f9636e95d1b010a6cbfa0293ffead3
adominguez <dmartin.alberto@gmail.com>
2024-01-31 19:08:07 +0100

    Add language support for new URLs

--- src/i18n/constants.ts
+++ src/i18n/constants.ts
@@ -5,5 +5,7 @@ interface ILanguages {
 export const LANGUAGES = {
   'localhost:4321': 'en',
   'localhost:4322': 'es',
+  'portico-multicountry-web-adominguez.vercel.app': 'en',
+  'portico-multicountry-web.vercel.app': 'es',
   DEFAULT: 'en',
 } as ILanguages
