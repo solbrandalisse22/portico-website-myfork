da58124a9f818e9e27afdac3a8b265264d8a6629
adominguez <dmartin.alberto@gmail.com>
2024-03-16 19:01:49 +0100

    refactor: i18n pages

--- src/shared/i18n/constants.ts
+++ src/shared/i18n/constants.ts
@@ -15,83 +15,83 @@ export const LANGUAGES = {
 export const courtsGrid = (i18n: any) => {
   return [
     {
-      title: i18n.COURTS.COURT1.TITLE,
-      description: i18n.COURTS.COURT1.LITTLE_DESCRIPTION,
-      url: i18n.COURTS.COURT1.URL.href,
-      label: i18n.COURTS.COURT1.URL.label,
+      title: i18n.PAGES.COURTS.COURT1.TITLE,
+      description: i18n.PAGES.COURTS.COURT1.LITTLE_DESCRIPTION,
+      url: i18n.PAGES.COURTS.COURT1.ROUTE.URL,
+      label: i18n.PAGES.COURTS.COURT1.ROUTE.LABEL,
       image: {
-        src: i18n.COURTS.COURT1.CONTACT_SECTION.IMAGE,
-        alt: i18n.COURTS.COURT1.NAME,
+        src: i18n.PAGES.COURTS.COURT1.CONTACT_SECTION.IMAGE,
+        alt: i18n.PAGES.COURTS.COURT1.NAME,
       }
     },
     {
-      title: i18n.COURTS.COURT2.TITLE,
-      description: i18n.COURTS.COURT2.LITTLE_DESCRIPTION,
-      url: i18n.COURTS.COURT2.URL.href,
-      label: i18n.COURTS.COURT2.URL.label,
+      title: i18n.PAGES.COURTS.COURT2.TITLE,
+      description: i18n.PAGES.COURTS.COURT2.LITTLE_DESCRIPTION,
+      url: i18n.PAGES.COURTS.COURT2.ROUTE.URL,
+      label: i18n.PAGES.COURTS.COURT2.ROUTE.LABEL,
       image: {
-        src: i18n.COURTS.COURT2.CONTACT_SECTION.IMAGE,
-        alt: i18n.COURTS.COURT2.NAME,
+        src: i18n.PAGES.COURTS.COURT2.CONTACT_SECTION.IMAGE,
+        alt: i18n.PAGES.COURTS.COURT2.NAME,
       }
     },
     {
-      title: i18n.COURTS.COURT3.TITLE,
-      description: i18n.COURTS.COURT3.LITTLE_DESCRIPTION,
-      url: i18n.COURTS.COURT3.URL.href,
-      label: i18n.COURTS.COURT3.URL.label,
+      title: i18n.PAGES.COURTS.COURT3.TITLE,
+      description: i18n.PAGES.COURTS.COURT3.LITTLE_DESCRIPTION,
+      url: i18n.PAGES.COURTS.COURT3.ROUTE.URL,
+      label: i18n.PAGES.COURTS.COURT3.ROUTE.LABEL,
       image: {
-        src: i18n.COURTS.COURT3.CONTACT_SECTION.IMAGE,
-        alt: i18n.COURTS.COURT3.NAME,
+        src: i18n.PAGES.COURTS.COURT3.CONTACT_SECTION.IMAGE,
+        alt: i18n.PAGES.COURTS.COURT3.NAME,
       }
     },
     {
-      title: i18n.COURTS.COURT4.TITLE,
-      description: i18n.COURTS.COURT4.LITTLE_DESCRIPTION,
-      url: i18n.COURTS.COURT4.URL.href,
-      label: i18n.COURTS.COURT4.URL.label,
+      title: i18n.PAGES.COURTS.COURT4.TITLE,
+      description: i18n.PAGES.COURTS.COURT4.LITTLE_DESCRIPTION,
+      url: i18n.PAGES.COURTS.COURT4.ROUTE.URL,
+      label: i18n.PAGES.COURTS.COURT4.ROUTE.LABEL,
       image: {
-        src: i18n.COURTS.COURT4.CONTACT_SECTION.IMAGE,
-        alt: i18n.COURTS.COURT4.NAME,
+        src: i18n.PAGES.COURTS.COURT4.CONTACT_SECTION.IMAGE,
+        alt: i18n.PAGES.COURTS.COURT4.NAME,
       }
     },
     {
-      title: i18n.COURTS.COURT5.TITLE,
-      description: i18n.COURTS.COURT5.LITTLE_DESCRIPTION,
-      url: i18n.COURTS.COURT5.URL.href,
-      label: i18n.COURTS.COURT5.URL.label,
+      title: i18n.PAGES.COURTS.COURT5.TITLE,
+      description: i18n.PAGES.COURTS.COURT5.LITTLE_DESCRIPTION,
+      url: i18n.PAGES.COURTS.COURT5.ROUTE.URL,
+      label: i18n.PAGES.COURTS.COURT5.ROUTE.LABEL,
       image: {
-        src: i18n.COURTS.COURT5.CONTACT_SECTION.IMAGE,
-        alt: i18n.COURTS.COURT5.NAME,
+        src: i18n.PAGES.COURTS.COURT5.CONTACT_SECTION.IMAGE,
+        alt: i18n.PAGES.COURTS.COURT5.NAME,
       }
     },
     {
-      title: i18n.COURTS.COURT6.TITLE,
-      description: i18n.COURTS.COURT6.LITTLE_DESCRIPTION,
-      url: i18n.COURTS.COURT6.URL.href,
-      label: i18n.COURTS.COURT6.URL.label,
+      title: i18n.PAGES.COURTS.COURT6.TITLE,
+      description: i18n.PAGES.COURTS.COURT6.LITTLE_DESCRIPTION,
+      url: i18n.PAGES.COURTS.COURT6.ROUTE.URL,
+      label: i18n.PAGES.COURTS.COURT6.ROUTE.LABEL,
       image: {
-        src: i18n.COURTS.COURT6.CONTACT_SECTION.IMAGE,
-        alt: i18n.COURTS.COURT6.NAME,
+        src: i18n.PAGES.COURTS.COURT6.CONTACT_SECTION.IMAGE,
+        alt: i18n.PAGES.COURTS.COURT6.NAME,
       }
     },
     {
-      title: i18n.COURTS.COURT7.TITLE,
-      description: i18n.COURTS.COURT7.LITTLE_DESCRIPTION,
-      url: i18n.COURTS.COURT7.URL.href,
-      label: i18n.COURTS.COURT7.URL.label,
+      title: i18n.PAGES.COURTS.COURT7.TITLE,
+      description: i18n.PAGES.COURTS.COURT7.LITTLE_DESCRIPTION,
+      url: i18n.PAGES.COURTS.COURT7.ROUTE.URL,
+      label: i18n.PAGES.COURTS.COURT7.ROUTE.LABEL,
       image: {
-        src: i18n.COURTS.COURT7.CONTACT_SECTION.IMAGE,
-        alt: i18n.COURTS.COURT7.NAME,
+        src: i18n.PAGES.COURTS.COURT7.CONTACT_SECTION.IMAGE,
+        alt: i18n.PAGES.COURTS.COURT7.NAME,
       }
     },
     {
-      title: i18n.COURTS.COURT8.TITLE,
-      description: i18n.COURTS.COURT8.LITTLE_DESCRIPTION,
-      url: i18n.COURTS.COURT8.URL.href,
-      label: i18n.COURTS.COURT8.URL.label,
+      title: i18n.PAGES.COURTS.COURT8.TITLE,
+      description: i18n.PAGES.COURTS.COURT8.LITTLE_DESCRIPTION,
+      url: i18n.PAGES.COURTS.COURT8.ROUTE.URL,
+      label: i18n.PAGES.COURTS.COURT8.ROUTE.LABEL,
       image: {
-        src: i18n.COURTS.COURT8.CONTACT_SECTION.IMAGE,
-        alt: i18n.COURTS.COURT8.NAME,
+        src: i18n.PAGES.COURTS.COURT8.CONTACT_SECTION.IMAGE,
+        alt: i18n.PAGES.COURTS.COURT8.NAME,
       }
     }
   ] as CourtItem[];
