1b5e22845b449085ffd2c5f10a802e91dd1771a6
adominguez <dmartin.alberto@gmail.com>
2024-04-19 17:44:29 +0200

    feat: add sitemap

--- astro.config.shared.mjs
+++ astro.config.shared.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import preact from "@astrojs/preact";
+import sitemap from '@astrojs/sitemap';
 
 import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), auth()],
+  integrations: [tailwind(), preact(), auth(), sitemap()],
   output: "server",
   adapter: vercel()
 });
