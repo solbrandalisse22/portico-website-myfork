1979a846ca955122c74fbfb11b1a1727265b016b
adominguez <dmartin.alberto@gmail.com>
2024-07-02 16:36:11 +0200

    feat add cleanUrls

--- astro.config.shared.mjs
+++ astro.config.shared.mjs
@@ -3,10 +3,27 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import preact from "@astrojs/preact";
 import sitemap from '@astrojs/sitemap';
+import cookieconsent from "@jop-software/astro-cookieconsent";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), sitemap()],
+  integrations: [tailwind(), preact(), sitemap(), cookieconsent({
+    guiOptions: {
+      consentModal: {
+        layout: 'cloud',
+        position: 'bottom center',
+        equalWeightButtons: true,
+        flipButtons: false,
+      },
+      preferencesModal: {
+        layout: "box",
+        position: "right",
+        equalWeightButtons: true,
+        flipButtons: false,
+      },
+    },
+  }),],
+  cleanUrls: true,
   output: "server",
   adapter: vercel(),
 });
