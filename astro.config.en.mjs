c6e5776a6c47d09cf30e38576559c9bdda6bfc65
solbrandalisse22 <92050645+solbrandalisse22@users.noreply.github.com>
2024-09-30 10:56:13 -0300

    Update astro.config.en.mjs

--- astro.config.en.mjs
+++ astro.config.en.mjs
@@ -341,7 +341,6 @@ export default defineConfig({
     "/wp-content/plugins/gtranslate/url_addon/gtranslate.php?glang=ru&gurl=customizacion": "/",
     "/mantenimiento": "/",
     "/privacy-policy": "/privacy",
-    "/outdoor-padel-courts":"/outdoor",
     "/fr/portico-sport-belgica": "/news/portico-sport-belgica",
     "/pistas-de-padel/amp": "/",
     "/pt/trabajos-integrados-en-pista-de-padel-de-hotel-spa-rural": "/news/trabajos-integrados-en-pista-de-padel-de-hotel-spa-rural",
