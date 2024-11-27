f7afc07562587b298b6282c3c3f29fcc7f607367
solbrandalisse22 <92050645+solbrandalisse22@users.noreply.github.com>
2024-11-27 11:52:59 -0300

    Create astro.config.mx.mjs

---
+++ astro.config.mx.mjs
@@ -0,0 +1,44 @@
+import shared from "./astro.config.shared.mjs";
+import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import { getCustomPages, getI18N } from "./src/shared/i18n/index";
+
+const language = 'en';
+const customPages = await getCustomPages({ language })
+
+export default defineConfig({
+  ...shared,
+  srcDir: "./src/es",
+  redirects: {
+    "/nuestra-fabrica":"/nuestra-fabrica",
+  },
+  
+  i18n: {
+    defaultLocale: "en",
+    locales: ["es", "en", "fr", "it", "de"],
+    routing: {
+      prefixDefaultLocale: false
+    },
+    domains: {
+      de: "https://www.porticosport.de",
+      fr: "https://www.porticosport.fr",
+      es: "https://www.porticosport.es",
+      it: "https://www.porticosport.it"
+    }
+  },
+  experimental: {
+    i18nDomains: true
+  },
+  integrations: [tailwind(), preact(), sitemap({
+    serialize(item) {
+      const lastCharacter = item.url.slice(-1);
+      if (lastCharacter === "/") {
+        item.url = item.url.slice(0, -1);
+      }
+      return item;
+    },
+    customPages,
+  }),],
+});
