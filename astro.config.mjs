1469e3ef5110a90d00059c0799cc5e8c9b71e21b
solbrandalisse22 <92050645+solbrandalisse22@users.noreply.github.com>
2025-01-06 11:02:06 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap({
-    exclude:['/elevated-platform', 'news/5-key-benefits-of-portico-sport-canopies-for-sports-and-tennis-clubs']
-  })]
+  integrations: [sitemap()]
 });
