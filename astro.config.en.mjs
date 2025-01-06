5a403d52784158fda1bd02c9c2bf2772bbabe837
solbrandalisse22 <92050645+solbrandalisse22@users.noreply.github.com>
2025-01-06 11:21:15 -0300

    Update astro.config.en.mjs

--- astro.config.en.mjs
+++ astro.config.en.mjs
@@ -503,7 +503,7 @@ export default defineConfig({
     i18nDomains: true
   },
   integrations: [tailwind(), preact(), sitemap({
-    exclude:['elevated-platform', 'news/5-key-benefits-of-portico-sport-canopies-for-sports-and-tennis-clubs'],
+    exclude:['https://www.porticosport.com/elevated-platform', 'https://www.porticosport.com/news/5-key-benefits-of-portico-sport-canopies-for-sports-and-tennis-clubs'],
     serialize(item) {
       const lastCharacter = item.url.slice(-1);
       if (lastCharacter === "/") {
