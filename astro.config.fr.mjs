ddbb68020c68f4f7790556824e4a69488310a0e7
solbrandalisse22 <92050645+solbrandalisse22@users.noreply.github.com>
2025-01-09 19:01:58 -0300

    Update astro.config.fr.mjs

--- astro.config.fr.mjs
+++ astro.config.fr.mjs
@@ -47,6 +47,24 @@ export default defineConfig({
       if (lastCharacter === "/") {
         item.url = item.url.slice(0, -1);
       }
+
+         const excludePatterns = [
+    "https://www.porticosport.fr/distributeur-agree",
+    "https://www.porticosport.fr/cookies",
+    "https://www.porticosport.fr/contact-client",
+    "https://www.porticosport.fr/legal-advice",
+    "https://www.porticosport.fr/notre-usine",
+    "https://www.porticosport.fr/privacy",
+    "https://www.porticosport.fr/projets",
+    "https://www.porticosport.fr/travailler-avec-nous"
+    
+  ];
+
+  for (const pattern of excludePatterns) {
+   
+      if (item.url === pattern) {
+        return null;  
+      }}
       return item;
     },
     customPages,
