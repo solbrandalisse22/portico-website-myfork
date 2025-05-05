0b71952f3fa5f51a3570d91e9a093b89a39ec275
solbrandalisse22 <92050645+solbrandalisse22@users.noreply.github.com>
2025-05-05 10:58:16 -0300

    Update news-sitemap.xml.ts

--- src/en/pages/padel-news/news-sitemap.xml.ts
+++ src/en/pages/padel-news/news-sitemap.xml.ts
@@ -1,44 +1,33 @@
-
 const category = 70;
 
 export const GET = async () => {
-  
   const response = await fetch(
     `https://portico.porticosport.com/wp-json/wp/v2/posts?categories=${category}&per_page=100&_embed`
   );
   const posts = await response.json();
 
-  const now = new Date();
-  const twoDaysAgo = new Date(now.getTime() - 48 * 60 * 60 * 1000); 
+  const newsItems = posts.map(post => {
+    const slug = post.slug;
+    const title = post.title?.rendered || "";
+    const publicationDate = new Date(post.date).toISOString();
+    const keywords = post.yoast_head_json?.keywords || "";
+    const authorName = post._embedded?.author[0]?.name || "Desconocido";
 
-  const newsItems = posts
-    .filter(post => {
-      const publishedDate = new Date(post.date);
-      return publishedDate > twoDaysAgo;
-    })
-    .map(post => {
-      const slug = post.slug;
-      const title = post.title?.rendered || "";
-      const publicationDate = new Date(post.date).toISOString(); 
-      const keywords = post.yoast_head_json?.keywords || ""; 
-      const authorName = post._embedded?.author[0]?.name || "Desconocido"; 
-
-      return `
-        <url>
-          <loc>https://porticosport.com/news/${slug}/</loc>
-          <news:news>
-            <news:publication>
-              <news:name>${authorName}</news:name>
-              <news:language>es</news:language>
-            </news:publication>
-            <news:publication_date>${publicationDate}</news:publication_date>
-            <news:title><![CDATA[${title}]]></news:title>
-            <news:keywords><![CDATA[${keywords}]]></news:keywords>
-          </news:news>
-        </url>
-      `;
-    })
-    .join('');
+    return `
+      <url>
+        <loc>https://porticosport.com/news/${slug}/</loc>
+        <news:news>
+          <news:publication>
+            <news:name>${authorName}</news:name>
+            <news:language>es</news:language>
+          </news:publication>
+          <news:publication_date>${publicationDate}</news:publication_date>
+          <news:title><![CDATA[${title}]]></news:title>
+          <news:keywords><![CDATA[${keywords}]]></news:keywords>
+        </news:news>
+      </url>
+    `;
+  }).join('');
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
